726ed4b09f37333e5bab4da62643fe63e63b59a4
nearbycoder <nearbycoder@gmail.com>
2016-12-22 14:38:38 -0600

    update dotenv to work with rspec

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'dotenv'
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+Dotenv.load
 require "fuel"
 
 module Dummy

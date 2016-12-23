f75845a4e874873aa4f37d24e5c02a7d33ac27c8
nearbycoder <nearbycoder@gmail.com>
2016-12-23 16:16:50 -0600

    Adds dependent: :destroy on posts for tags
    In case of a post getting deleted we want to remove all tags associated
    with that post on the join table.

--- app/models/fuel/post.rb
+++ app/models/fuel/post.rb
@@ -9,7 +9,7 @@ module Fuel
 
     belongs_to :category
 
-    has_many :post_tags
+    has_many :post_tags, dependent: :destroy
     has_many :tags, through: :post_tags
 
     if Rails.version[0].to_i < 4

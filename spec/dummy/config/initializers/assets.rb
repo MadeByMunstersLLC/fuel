a44ceefb6b5330157fad986498e8de9893b8f407
nearbycoder <nearbycoder@gmail.com>
2016-12-23 12:22:40 -0600

    Adds new sidebar icons and new links to category and tag routes

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( fuel/logo.svg )
 Rails.application.config.assets.precompile += %w( fuel/default-img.jpg )
+Rails.application.config.assets.precompile += ['fuel/icons/*']

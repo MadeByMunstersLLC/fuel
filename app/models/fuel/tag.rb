8c92d24b8ce9cbd8d0c2579103fd7213751792b5
nearbycoder <nearbycoder@gmail.com>
2017-01-03 18:43:10 -0600

    Adds friendly_id mixin and helper to category and tag models

--- app/models/fuel/tag.rb
+++ app/models/fuel/tag.rb
@@ -3,6 +3,9 @@ module Fuel
     has_many :post_tags, dependent: :destroy
     has_many :posts, through: :post_tags
 
+    extend FriendlyId
+    friendly_id :name, use: :slugged
+
     if Rails.version[0].to_i < 4
       attr_accessible :name
     end

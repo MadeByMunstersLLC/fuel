e5169a7a8d2d890042ec2ab2360762e099d71d1b
nearbycoder <nearbycoder@gmail.com>
2017-01-03 18:46:16 -0600

    Fix initial implementation by removing posts being rendered with the category json.

--- app/controllers/fuel/categories_controller.rb
+++ app/controllers/fuel/categories_controller.rb
@@ -4,7 +4,7 @@ module Fuel
       @categories = Fuel::Category.all
 
       respond_to do |format|
-        format.json { render json: @categories, includes: [:posts] }
+        format.json { render json: @categories }
       end
     end
   end
